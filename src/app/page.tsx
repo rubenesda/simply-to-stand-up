0806b37ade6947d2800bf5bb9ceed8e28a0cb69d
Ruben Dario Escandon <rubenesda@gmail.com>
2024-05-26 18:40:50 -0500

    add(ui-components): add static components to one single page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import List from "./components/list";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      My first change
+    <main className="flex flex-col justify-center">
+      <form className="flex flex-row justify-center py-8">
+        {/* Generator */}
+        <label className="mr-2">Hours: </label>
+        <input className="border-2 mr-10 rounded placeholder:italic placeholder:text-slate-400" placeholder="Type number of hours"></input>
+        <button className="hover:bg-gray-100 border-2 border-current rounded-md px-4" type="submit">Generate</button>
+      </form>
+      <div className="flex flex-row justify-center min-h-screen py-8">
+        <List title="To-Do"/>
+        <List title="Done"/>
+      </div>
     </main>
   );
 }
